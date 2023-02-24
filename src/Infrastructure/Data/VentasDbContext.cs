129aae907db94e9c04bd1ea787c0d758e0176755
CeramicCodes2 <ivansanpan@gmail.com>
2023-02-24 17:32:53 -0600

    s

--- src/Infrastructure/Data/VentasDbContext.cs
+++ src/Infrastructure/Data/VentasDbContext.cs
@@ -68,16 +68,40 @@ CREATE TABLE VENTAS(
     public Venta Details(Guid id)
     {
         var data = new Venta();
-
+        var con = new SqlConnection(_connectionString);
+        var cmd = new SqlCommand("SELECT [V].[Id],[V].[ClienteId],[C].[Nombre],[C].[Direccion],[C].[Correo],[C].[Telefono],[V].[ProductoId],[P].[Descripcion],[P].[Precio],[P].[Cantidad],[V].[Fecha] FROM [Venta] AS [V] INNER JOIN [Cliente] [C] ON [V].[ClienteId]=[C].[Id] INNER JOIN  [PRODUCTO] AS [P] ON [P].[Id]=[V].[ProductoId]",con);
         // ToDo
+        cmd.Parameters.Add("@Id",SqlDbType.UniqueIdentifier).Value = id;
         try
         {
+            con.Open();
+            var dr = cmd.ExecuteReader();
+            if(dr.Read()){
+                data.Id = (Guid)dr["Id"];
+                data.ClienteId = (Guid)dr["ClienteId"];
+                data.Cliente = new Cliente{
+                    Id = (Guid)dr["Id"],// convertimos el dato id a guid
+                    Nombre = (string)dr["Nombre"],
+                    Direccion =  (string)dr["Direccion"],
+                    Telefono = (string)dr["Telefono"],
+                    Correo = (string)dr["Correo"]
+                };
+                data.ProductoId = (Guid)dr["ProductoId"];
+                data.Producto = new Producto{
+                    Id = (Guid)dr["Id"],
+                    Descripcion = (string)dr["Descripcion"],
+                    Precio = (decimal)dr["Precio"],
+                    Cantidad = (int)dr["Cantidad"]
+                };
+                data.Fecha = (DateTime)dr["Fecha"];
+            }
             // ToDo
             return data;
         }
         catch (Exception) { throw; }
         finally
         {
+            con.Close();
             // ToDo
         }
     }
